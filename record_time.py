d2458bbe20c2b1e109f5aa1f6a61d8815f370f9f
ichizokichinosuke <nuuuukemw@gmail.com>
2020-05-19 15:52:46 +0900

    add work time

--- record_time.py
+++ record_time.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import paho.mqtt.client as mqtt
 
-COLUMNS = ["Work_start", "Rest_start", "Rest_end", "Work_end"]
+COLUMNS = ["Work_start", "Rest_start", "Rest_end", "Work_end", "Work_time"]
 PERIODS = 50
 CSV_PATH = "./work_table.csv"
 
@@ -24,11 +24,13 @@ def on_message(client, userdata, msg):
     data = json.loads(msg.payload.decode("utf-8"))["data"][0]
     data = {key:value.strip() for key, value in data.items()}
     print(data)
+
     if "スタート" == data["what"] or "開始" == data["what"]:
         print("Start working!")
         start_dt = datetime.datetime.now()
-        start_day = start_dt.strftime("%Y-%m-%d")
+        start_day = start_dt.date()
         start_time = start_dt.strftime("%X")
+        # start_time = start_dt.time()
         time_df.loc[start_day, "Work_start"] = start_time
 
     elif "終わり" == data["what"] or "終了" == data["what"]:
@@ -38,19 +40,24 @@ def on_message(client, userdata, msg):
         end_time = end_dt.strftime("%X")
         time_df.loc[end_day, "Work_end"] = end_time
 
+        start_time = time_df.at[end_day, "Work_start"]
+        start_time = datetime.datetime.strptime(start_time, "%X")
+        end_time = datetime.datetime.strptime(end_time, "%X")
+
+        time_df.loc[end_day, "Work_time"] = end_time - start_time
+
     time_df.to_csv(CSV_PATH)
     print(time_df.head())
 
 
+# if os.path.exists(CSV_PATH):
+#     time_df = pd.read_csv(CSV_PATH, index_col=0)
 
-if os.path.exists(CSV_PATH):
-    time_df = pd.read_csv(CSV_PATH, index_col=0)
-
-else:
-    ini_mat = np.empty((PERIODS, 4))
-    ini_mat[:,:] = np.nan
-    date_index = pd.date_range("2020-5-15", periods=PERIODS, freq="D")
-    time_df = pd.DataFrame(ini_mat, index=date_index, columns=COLUMNS)
+# else:
+ini_mat = np.empty((PERIODS, len(COLUMNS)))
+ini_mat[:,:] = np.nan
+date_index = pd.date_range("2020-5-15", periods=PERIODS, freq="D")
+time_df = pd.DataFrame(ini_mat, index=date_index, columns=COLUMNS)
 
 client = mqtt.Client()
 client.username_pw_set("token:%s"%TOKEN)
