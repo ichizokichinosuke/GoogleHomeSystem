6801617023f8aba9b0e8074993ef6d4eac695539
ichizokichinosuke <nuuuukemw@gmail.com>
2020-05-29 19:17:14 +0900

    record work time and completely no error about value type

--- record_time.py
+++ record_time.py
@@ -7,10 +7,11 @@ import paho.mqtt.client as mqtt
 
 COLUMNS = ["Work_start", "Rest_start", "Rest_end", "Work_end", "Work_time"]
 WORK_INSTRUCTION = ["勤務", "仕事", "ワーク",]
-REST_INSTRUCTION = ["休憩", "中断", "一休み"]
+REST_INSTRUCTION = ["休憩", "中断", "離席",]
 START_INSTRUCTION = ["スタート", "開始", "始め"]
 END_INSTRUCTION = ["エンド", "終了", "やめ"]
 PERIODS = 50
+REST_HOUR = 1
 CSV_PATH = "./work_table.csv"
 
 TOKEN = "token_iwZlPmqHXaxtbY3q"
@@ -18,6 +19,7 @@ HOSTNAME = "mqtt.beebotte.com"
 PORT = 8883
 TOPIC = "LocalTest/voice"
 CACERT = "mqtt.beebotte.com.pem"
+
 
 def on_connect(client, userdata, flags, respons_code):
     print('status {0}'.format(respons_code))
@@ -29,16 +31,45 @@ def on_message(client, userdata, msg):
     data = {key:value.strip() for key, value in data.items()}
     print(data)
     work_or_rest, start_or_end = data["what"].split()
-    print(work_or_rest, start_or_end)
 
     record_time(what=work_or_rest, how=start_or_end)
 
     time_df.to_csv(CSV_PATH)
     print(time_df.head())
 
+def calc_work_hour(day, work_end_time):
+    null_df = time_df.isnull()
+    work_end_time = datetime.datetime.strptime(work_end_time, "%X")
+    print(work_end_time)
+
+    if not null_df.at[day, "Work_start"]:
+        work_start_time = time_df.at[day, "Work_start"]
+        work_start_time = datetime.datetime.strptime(work_start_time, "%X")
+    else:
+        return 0
+
+    if not null_df.at[day, "Rest_start"] and not null_df.at[day, "Rest_end"]:
+        rest_start_time = time_df.at[day, "Rest_start"]
+        rest_end_time = time_df.at[day, "Rest_end"]
+        rest_start_time = datetime.datetime.strptime(rest_start_time, "%X")
+        rest_end_time = datetime.datetime.strptime(rest_end_time, "%X")
+
+        time_df.at[day, "Work_time"] = work_end_time - work_start_time - (rest_end_time-rest_start_time)
+
+    else:
+        if str(work_end_time - work_start_time)[0] == "0":
+            one_hour = datetime.timedelta(hours=0)
+        else:
+            one_hour = datetime.timedelta(hours=REST_HOUR)
+
+        time_df.at[day, "Work_time"] = work_end_time - work_start_time - one_hour
+    # rest_time = rest_end_time - rest_start_time
+
+
 def record_time(what="work", how="start"):
     dt = datetime.datetime.now()
     day = dt.date()
+    day = day.strftime("%Y-%m-%d")
     time = dt.strftime("%X")
     # start_time = start_dt.time()
     if what in WORK_INSTRUCTION:
@@ -48,25 +79,14 @@ def record_time(what="work", how="start"):
         elif how in END_INSTRUCTION:
             print("Work End!")
             time_df.at[day, "Work_end"] = time
+            calc_work_hour(day, time)
 
-            work_start_time = time_df.at[day, "Work_start"]
-            rest_start_time = time_df.at[day, "Rest_start"]
-            rest_end_time = time_df.at[day, "Rest_end"]
-            work_start_time = datetime.datetime.strptime(work_start_time, "%X")
-            rest_start_time = datetime.datetime.strptime(rest_start_time, "%X")
-            rest_end_time = datetime.datetime.strptime(rest_start_time, "%X")
-
-            one_hour = datetime.time(hour=1)
-            rest_time = rest_end_time - rest_start_time
-            rest_time = one_hour if rest_time < one_hour else rest_time
-
-            end_time = datetime.datetime.strptime(time, "%X")
-            time_df.at[day, "Work_time"] = end_time - start_time - rest_time
         else:
             print(" Error! When record about work.")
     elif what in REST_INSTRUCTION:
         if how in START_INSTRUCTION:
             print("Rest Start!")
+            print(day)
             time_df.at[day, "Rest_start"] = time
         elif how in END_INSTRUCTION:
             print("Rest End!")
@@ -76,13 +96,14 @@ def record_time(what="work", how="start"):
 
 def load_table():
     if os.path.exists(CSV_PATH):
-        time_df = pd.read_csv(CSV_PATH, index_col=0)
+        print("Loading..")
+        time_df = pd.read_csv(CSV_PATH, index_col=0, dtype=str)
 
     else:
         ini_mat = np.empty((PERIODS, len(COLUMNS)))
         ini_mat[:,:] = np.nan
         date_index = pd.date_range(datetime.date.today(), periods=PERIODS, freq="D")
-        time_df = pd.DataFrame(ini_mat, index=date_index, colusmn=COLUMNS)
+        time_df = pd.DataFrame(ini_mat, index=date_index, columns=COLUMNS)
 
     return time_df
 
